8cefe472cb345e28c727288f2f9327924dc29095
Momo147-labe <fodemomos11@gmail.com>
2025-12-31 22:17:14 +0000

    base ajouter

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Pour parser les JSON
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Import des routes
 const usersRouter = require("./routes/users.routes");
